8433a89395944857caea78cd53f45b9bb1f66ca3
Luan Bitar <7@lbitar.com>
2020-02-12 18:47:03 -0300

    feat: fixing domaing environment issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,25 @@
-const path = require(`path`);
+const path = require(`path`)
 
 module.exports = {
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-alias-imports`,
-    `gatsby-plugin-provide-react`,
-    `gatsby-env-variables`,
-    `gatsby-plugin-dynamic-routes`,
-    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-alias-imports`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-provide-react`,
+    `gatsby-plugin-dynamic-routes`,
     {
-      resolve: "gatsby-plugin-sass",
+      resolve: `gatsby-plugin-sass`,
       options: {
-        data: `@import "${__dirname}/src/styles/index.scss";`
-      }
+        data: `@import "${__dirname}/src/styles/index.scss";`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: path.join(__dirname, `src`, `images`)
-      }
+        path: path.join(__dirname, `src`, `images`),
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -28,17 +27,17 @@ module.exports = {
         name: `Rita RH`,
         short_name: `Rita RH`,
         start_url: `/`,
-        icon: `src/images/gatsby-icon.png`
-      }
+        icon: `src/images/gatsby-icon.png`,
+      },
     },
     {
       resolve: `gatsby-plugin-page-creator`,
       options: {
         path: `${__dirname}/src/pages`,
         ignore: {
-          patterns: [`**/*`]
-        }
-      }
-    }
-  ]
-};
+          patterns: [`**/*`],
+        },
+      },
+    },
+  ],
+}
--- src/pages/ListUsers/logic/effects.js
+++ src/pages/ListUsers/logic/effects.js
@@ -1,16 +1,16 @@
 import axios from "axios"
 import { useEffect } from "react"
 
+const { GATSBY_API_ROOT_URL } = process.env
+const USERS_URL = `${GATSBY_API_ROOT_URL || "/api"}/users`
+
 export default function({ setUsers }) {
   useEffect(() => {
     async function getUsers() {
-      const { data } = await axios.post(
-        "https://juntos-somos-mais.bitar.now.sh/api/users",
-        {},
-      )
+      const { data } = await axios.post(USERS_URL, {})
       const { users } = data
       setUsers(users)
-      console.log("getting users", data)
+      console.log({ GATSBY_API_ROOT_URL, data })
     }
     getUsers()
   }, [])
