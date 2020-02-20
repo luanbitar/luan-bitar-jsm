8294738ca76f5dc457063f56280824413f7ebe4f
Luan Bitar <7@lbitar.com>
2020-02-19 21:48:45 -0300

    feat: fixing 404 page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -2,15 +2,22 @@ import { Helmet } from "react-helmet"
 
 import "./NotFound.scss"
 import Header from "components/Header"
+import Footer from "components/Footer"
+import NotFoundIllustration from "assets/il-404.svg"
 
 function NotFound() {
   return (
-    <div className="list-users-page">
+    <div className="not-found-page">
       <Helmet>
         <title>Juntos Somos +</title>
       </Helmet>
       <Header withSearch />
-      <h1>404 AEEEEEEEEE</h1>
+
+      <main className="container">
+        <NotFoundIllustration className="not-found-illustration" />
+      </main>
+
+      <Footer />
     </div>
   )
 }
